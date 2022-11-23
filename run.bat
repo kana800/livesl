df4b0c7f04115c8c321e9db2d60a2bfd780502c8
Chathura Nimesh <64830745+kana800@users.noreply.github.com>
2022-11-24 00:54:17 +0530

    test push on website
    - changed path in run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 
 ::checking if the environment is activated
-set virtual_loc=scraper\venv\Scripts\activate
+set virtual_loc=venv\Scripts\activate
 @echo off
 if "%VIRTUAL_ENV%"=="%VIRTUAL_ENV%" (%virtual_loc%) 
 
